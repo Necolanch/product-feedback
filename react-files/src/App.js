5a28acf64ea9f7b8943bfd7f1ca75138c145d071
Necolanch <NICruz@student.fullsail.edu>
2022-03-29 10:07:49 -0400

    Lift state up to App parent component

--- react-files/src/App.js
+++ react-files/src/App.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import TitlePiece from "./components/TitlePiece";
-//import PostsBoardHeader from "./components/PostsBoardHeader";
+import PostsBoardHeader from "./components/PostsBoardHeader";
 import {PostsBoard} from "./components/PostsBoard";
 
 
 function App() {
+  const [feedbackData, setFeedbackData]= useState([]);
+  const [isLoaded]=useState([{isLoaded:true}]);
+
+  useEffect(()=>{
+    async function getData() {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+
+        const feedback=data.map(fb=>{
+            return{
+                id:fb.id,
+                title:fb.title,
+                body:fb.body,
+                likes:fb.userId
+            }
+        })
+        setFeedbackData(feedback);
+    }
+
+    if({isLoaded}) {
+        getData();
+    } else {
+        console.log("Cannot load");
+    }
+}, [isLoaded])
+
+
+  const [sortState, setSortState]=useState("Select sort");
+
+  useEffect(()=> {
+    const sortByLikes = method => {
+        if(method === "Most likes") {
+        const sorted = [...feedbackData].sort((a,b)=> b.likes-a.likes);
+        setFeedbackData(sorted);
+      };
+    };
+
+    sortByLikes(sortState);
+  }, [sortState])
 
   return (
     <div className="App">
       <TitlePiece titleText="Product Feedback"></TitlePiece>
-        <PostsBoard></PostsBoard>
+        <PostsBoardHeader sort={setSortState}></PostsBoardHeader>
+        <PostsBoard posts={feedbackData}></PostsBoard>
     </div>
   );
 }
